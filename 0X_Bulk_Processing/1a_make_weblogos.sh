388a8eaf588bed4d3c346f6e86e25210d196ba03
Olivia Wen-Mei Lang <oliviawlang@gmail.com>
2024-03-09 13:13:47 -0500

    restructure bulk processing to use config files
    Restructure bulk processing to reduce sbatch scripts and rely on config files to enumerate each BED x BAM combo to run (per pileup type/sbatch script). Also added scripts for conservation and SNP pileups

--- 0X_Bulk_Processing/1a_make_weblogos.sh
+++ 0X_Bulk_Processing/1a_make_weblogos.sh
@@ -3,15 +3,20 @@
 # Make weblogos for every MEME file in `02_Call_RefPT/PWM/*.meme.txt`
 
 ### CHANGE ME
-WRK=/path/to/2023-Chen_Benzonase-ChIP-exo/0X_Bulk_Processing
+WRK=/path/to/2024-Chen_Nature/0X_Bulk_Processing
+WRK=/storage/home/owl5022/scratch/2024-Chen_Nature/0X_Bulk_Processing
 ###
 
 # Dependencies
 # - ceqlogo
 
+set -exo
+module load anaconda
+conda activate bx
+
 # Fill in placeholder constants with your directories
 PWM=$WRK/../02_Call_RefPT/PWM
-OUTDIR=$WRK/Library-WebLogos
+OUTDIR=$WRK/Library/WebLogos
 
 # Set up output directories
 [ -d $OUTDIR ] || mkdir -p $OUTDIR
@@ -21,6 +26,6 @@ for PWMFILE in $PWM/*.meme.txt;
 do
     BASE=`basename $PWMFILE ".meme.txt"`
     # Generate logo
-    ceqlogo -i $PWMFILE -o $OUTDIR/$BASE\_logo.eps -f EPS
+    ceqlogo -i $PWMFILE -m 1 -o $OUTDIR/$BASE\_logo.eps -f EPS
 done
 
