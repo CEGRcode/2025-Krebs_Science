c4b15807d20ce41b0d35a1ea134ba84e4ecc44b3
Olivia Wen-Mei Lang <oliviawlang@gmail.com>
2024-03-11 04:11:13 -0400

    update WDR5 to TSS distance RefPT
    Mimic the TSS RefPT building from Haining's Nature branch
    - add module load and dependency statements
    - fix filepaths (down extra directory level or change BED to RefPT-Other)

--- 02_Call_RefPT/5_WDR5TSS_sort.sh
+++ 02_Call_RefPT/5_WDR5TSS_sort.sh
@@ -2,54 +2,61 @@
 
 ### CHANGE ME
 WINDOW=100
-WRK=/path/to/2023-Chen_Benzonase-ChIP-exo/02_Call_RefPT
+WRK=/path/to/2024-Chen_Nature/02_Call_RefPT
+WRK=/storage/home/owl5022/scratch/2024-Chen_Nature/02_Call_RefPT
 ###
 
 # Dependencies
 # - java
 # - python
+# - bedtools
 
 set -exo
 module load anaconda3
-source activate bioinfo
+module load bedtools
+source activate bx
 
 # Script shortcuts
-SCRIPTMANAGER=$WRK/bin/ScriptManager-v0.14.jar
+SCRIPTMANAGER=$WRK/../bin/ScriptManager-v0.14.jar
 
 # Inputs and outputs
-BLACKLIST=$WRK/data/hg19_files/hg19_exclude.bed
-MOTIF=$WRK/data/RefPT-Motif
+BLACKLIST=$WRK/../data/hg19_files/hg19_exclude.bed
+MOTIF=$WRK/../data/RefPT-Motif
 
 # find top 100 sites of WDR5_M1
 head -n 100 FIMO/WDR5/WDR5_MOTIF1_sorted.bed | awk '{OFS="\t"} {print $1,$2,$3,$1"_"$2"_"$3,$6,$7}' >  $MOTIF/WDR5_Occupancy.bed
 # expand 1bp and sort
 java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1 $MOTIF/WDR5_Occupancy.bed -o $MOTIF/1bp/WDR5_Occupancy_1bp.bed
 bedtools sort -i $MOTIF/1bp/WDR5_Occupancy_1bp.bed > $MOTIF/1bp/WDR5_Occupancy_1bp_sort.bed
-# find TSS close to WDR5_M1
-java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1 $WRK/data/BED/tss_all_k562.bed -o $WRK/data/BED/tss_all_k562_1bp.bed
-bedtools sort -i $WRK/data/BED/tss_all_k562_1bp.bed | bedtools closest -a $MOTIF/1bp/WDR5_Occupancy_1bp_sort.bed -b - -d -D b -t first | awk '{print $7,$8,$9,$10,$11,$12,$1,$2,$3,$4,$5,$6,$13}' | sort -k13,13n > FIMO/WDR5/TSS_WDR5_sorted.bed
+# find Annotated TSS or GRO-cap defined all TSS close to WDR5_M1 
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1 $WRK/data/RefPT-Other/tss_all_k562.bed -o $WRK/data/RefPT-Other/tss_all_k562_1bp.bed
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1 $WRK/data/RefPT-Other/K562_CoPRO-expressed_Gene-refSeqTSS_1000bp.bed -o $WRK/data/RefPT-Other/K562_CoPRO-expressed_Gene-refSeqTSS_1bp.bed
+bedtools sort -i $WRK/data/RefPT-Other/tss_all_k562_1bp.bed | bedtools closest -a $MOTIF/1bp/WDR5_Occupancy_1bp_sort.bed -b - -d -D b -t first | awk '{print $7,$8,$9,$10,$11,$12,$1,$2,$3,$4,$5,$6,$13}' | sort -k13,13n | awk '{if (($13 <= 500) && ($13 >= -500) && ($7 != ".")) print $0 > "../data/RefPT-Other/TSS94_WDR5_sort.bed"}'
+bedtool sort -i $WRK/data/RefPT-Other/K562_CoPRO-expressed_Gene-refSeqTSS_1bp.bed | bedtools closest -a $MOTIF/1bp/WDR5_Occupancy_1bp_sort.bed -b - -d -D a -t first | sort -k13,13n | awk '{if (($13 <= 500) && ($13 >= -500) && ($2 != "-1")) print $0 > "../data/RefPT-Other/Annotated_TSS_WDR5_sort.bed"}'  
+
 # Clean-up
 rm $MOTIF/1bp/WDR5_Occupancy_1bp_sort.bed
-# Find TSS that are close to WDR5 less than 500bp
-awk '{if (($13 <= 500) && ($13 >= -500) && ($7 != ".")) print $0 > "../data/BED/TSS94_WDR5_sort.bed"}' FIMO/WDR5/TSS_WDR5_sorted.bed
-rm FIMO/WDR5/TSS_WDR5_sorted.bed
-java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 $MOTIF/WDR5_Occupancy.bed -o $MOTIF/1000bp/WDR5_Occupancy_1000bp.bed
-java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 ../data/BED/TSS94_WDR5_sort.bed -o ../data/BED/TSS94_WDR5_sort_1000bp.bed
 
-awk -v motif="$MOTIF" '{OFS="\t"} {print $7,$8,$9,$10,$11,$12,$1,$2,$3,$4,$5,$6,$13}' ../data/BED/TSS94_WDR5_sort.bed | \
+# expand all WDR5 and related TSS
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 $MOTIF/WDR5_Occupancy.bed -o $MOTIF/1000bp/WDR5_Occupancy_1000bp.bed
+java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 ../data/RefPT-Other/TSS94_WDR5_sort.bed -o ../data/RefPT-Other/TSS94_WDR5_sort_1000bp.bed
+
+# find WDR5 orientation
+awk -v motif="$MOTIF" '{OFS="\t"} {print $7,$8,$9,$10,$11,$12,$1,$2,$3,$4,$5,$6,$13}' ../data/RefPT-Other/TSS94_WDR5_sort.bed | \
 awk -v motif="$MOTIF" '{if ($6 == $12) print $0 > motif "/WDR5_TSS_same.bed"; else print $0 > motif "/WDR5_TSS_oppo.bed" }'
 
-awk '{if ($6 == $12) print $0 > "../data/BED/TSS_WDR5_same_1000bp.bed"; else print $0 > "../data/BED/TSS_WDR5_oppo_1000bp.bed" }' ../data/BED/TSS94_WDR5_sort_1000bp.bed
+# Selecting TSS with WDR5 same orientation
+awk '{if ($6 == $12) print $0 > "../data/RefPT-Other/TSS_WDR5_same_1000bp.bed"; else print $0 > "../data/RefPT-Other/TSS_WDR5_oppo_1000bp.bed" }' ../data/RefPT-Other/TSS94_WDR5_sort_1000bp.bed
 
+# Plot figure of WDR5 motif relative to TSS, orientation seperately
 java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 32 $MOTIF/WDR5_TSS_same.bed -o $MOTIF/WDR5_TSS_same_32bp.bed
 java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 32 $MOTIF/WDR5_TSS_oppo.bed -o $MOTIF/WDR5_TSS_oppo_32bp.bed
-java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 $MOTIF/WDR5_TSS_oppo.bed -o $MOTIF/1000bp/WDR5_TSS_oppo_1000bp.bed
 
-java -jar $SCRIPTMANAGER peak-analysis peak-align-ref -o FIMO/WDR5/WDR5_TSS_same_32bp_TSS94_WDR5_sort_1000bp.cdt $MOTIF/WDR5_TSS_same_32bp.bed ../data/BED/TSS94_WDR5_sort_1000bp.bed
+java -jar $SCRIPTMANAGER peak-analysis peak-align-ref -o FIMO/WDR5/WDR5_TSS_same_32bp_TSS94_WDR5_sort_1000bp.cdt $MOTIF/WDR5_TSS_same_32bp.bed ../data/RefPT-Other/TSS94_WDR5_sort_1000bp.bed
 
 java -jar $SCRIPTMANAGER figure-generation heatmap -a 1 --blue FIMO/WDR5/WDR5_TSS_same_32bp_TSS94_WDR5_sort_1000bp.cdt -o FIMO/WDR5/WDR5_TSS_same_32bp_TSS94_WDR5_sort_1000bp.png
 
-java -jar $SCRIPTMANAGER peak-analysis peak-align-ref -o FIMO/WDR5/WDR5_TSS_oppo_32bp_TSS94_WDR5_sort_1000bp.cdt $MOTIF/WDR5_TSS_oppo_32bp.bed ../data/BED/TSS94_WDR5_sort_1000bp.bed
+java -jar $SCRIPTMANAGER peak-analysis peak-align-ref -o FIMO/WDR5/WDR5_TSS_oppo_32bp_TSS94_WDR5_sort_1000bp.cdt $MOTIF/WDR5_TSS_oppo_32bp.bed ../data/RefPT-Other/TSS94_WDR5_sort_1000bp.bed
 
 java -jar $SCRIPTMANAGER figure-generation heatmap -a 1 --red FIMO/WDR5/WDR5_TSS_oppo_32bp_TSS94_WDR5_sort_1000bp.cdt -o FIMO/WDR5/WDR5_TSS_oppo_32bp_TSS94_WDR5_sort_1000bp.png
 
