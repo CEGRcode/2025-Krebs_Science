92bc07a59bb4357d3bd376e74feb8c585acb3521
HeninChen <46940074+HeninChen@users.noreply.github.com>
2024-10-17 02:33:55 -0400

    Update 4_NFIA_motif.sh

--- 02_Call_RefPT/4_NFIA_motif.sh
+++ 02_Call_RefPT/4_NFIA_motif.sh
@@ -4,7 +4,7 @@
 module load anaconda
 source activate bioinfo
 ### CHANGE ME
-WRK=/storage/group/bfp2/default/hxc585_HainingChen/Fox_NFIA_CTCF
+WRK=/Path/to/Title
 MOTIF=$WRK/data/RefPT-Motif/
 FIMO=$WRK/ 02_Call_RefPT/FIMO
 SCRIPTMANAGER=$WRK/bin/ScriptManager-v0.15.jar
@@ -13,7 +13,7 @@ Genome=$WRK/data/hg38_files/hg38.info.txt
 Nuc="$WRK/Motif_analysis/K562_Nucpeak_hg38liftover_sort.bed"
 
 NFIA1=$WRK/data/BAM/K562_NFIA_BX_rep1_hg38.bam
-K562IgG=$WRK/data/BAM/K562_IgG_BX_merge_temp_hg38.bam
+K562IgG=$WRK/data/BAM/K562_IgG_BX_merge_hg38.bam
 Input=$WRK/data/BAM/hg38_BAM/K562_-_BI_rep1_hg38.bam
 
 NFIA=$WRK/Motif_analysis/NFIA
@@ -22,17 +22,15 @@ mkdir -p $NFIA
 cd $NFIA
 
 # determine the nucleosome engagment direction on each NFIA sites
-
 awk '
 {
-    if ($1 !~/alt/ ) && ($1 !~/random/ )  {
+    if $1 !~/alt/ ) && ($1 !~/random/) && ($1 !~/Un/) {
         print $0 > "NFIA_Occupancy_1bp.bed";
     } 
 } ' $FIMO/NFIA/NFIA_Occupancy_1bp.bed
 
-
 wc -l NFIA_Occupancy_1bp.bed
-7965 NFIA_Occupancy_1bp.bed
+
 bedtools shift -i NFIA_Occupancy_1bp.bed -g $Genome -p 95 -m -95 > NFIA_Occupancy_down95bp.bed
 bedtools shift -i NFIA_Occupancy_1bp.bed -g $Genome -p -95 -m 95 > NFIA_Occupancy_up95bp.bed
 
@@ -44,7 +42,6 @@ for file in NFIA_Occupancy_down95bp.bed  NFIA_Occupancy_up95bp.bed ; do
   java -jar "$SCRIPTMANAGER" read-analysis tag-pileup ${filename}_150bp.bed "$NFIA1" -n 100 -1 --combined --cpu 4 -M SCORES/NFIA_${filename}_150bp_read1_MIN100
   rm ${filename}_150bp.bed
 done
-
 
 java -jar $SCRIPTMANAGER read-analysis aggregate-data --sum SCORES/NFIA_NFIA_Occupancy_up95bp_150bp_read1_MIN100_combined.cdt -o SCORES/
 java -jar $SCRIPTMANAGER read-analysis aggregate-data --sum SCORES/NFIA_NFIA_Occupancy_down95bp_150bp_read1_MIN100_combined.cdt -o SCORES/
@@ -64,9 +61,9 @@ awk '
 '
 
 wc -l NFIA_Nuc_upstream_temp.bed
-4093 NFIA_Nuc_upstream_temp.bed
+
 wc -l NFIA_Nuc_downstream_temp.bed
-3873 NFIA_Nuc_downstream_temp.bed
+
 
 awk '{OFS="\t"} {print $1,$2,$3,$4,$5,$6,$7,$8,$9,"engageNuddown"}' NFIA_Nuc_downstream_temp.bed  > NFIA_engagedNuc_downstream.bed
 awk '{OFS="\t"} {print $1,$2,$3,$4,$5,$6,$7,$8,$9,"engageNudup"}' NFIA_Nuc_upstream_temp.bed > NFIA_engagedNuc_upstream.bed
@@ -86,6 +83,9 @@ sort -k7,7nr > NFIA_downNuc.bed
 
 rm NFIA_Nuc_upstream_-_convert.bed NFIA_Nuc_upstream_+_convert.bed
 
+
+# Sort orientated NFIA by distance to closest nucleosome, seperate by up/down/overlap
+
 bedtools sort -i NFIA_downNuc.bed | uniq | bedtools closest -a - -b $Nuc -d -D a -t first | sort -k17,17n  > NFIA_engageNucdown_NucSort.bed 
 
 awk '{
@@ -100,11 +100,8 @@ awk '{
 
 
 wc -l NFIA_NucSort-*.bed
-1563 NFIA_NucSort-DOWNSTREAM.bed
-5248 NFIA_NucSort-OVERLAP.bed
-1155 NFIA_NucSort-UPSTREAM.bed
 
-## random strand
+## random NFIA nucleosome engagement score upstream/downstream on original NFIA motif
 cut -f 3-152 "SCORES/NFIA_NFIA_Occupancy_up95bp_150bp_read1_MIN100_combined.cdt" | \
 paste "SCORES/NFIA_NFIA_Occupancy_down95bp_150bp_read1_MIN100_combined.cdt" -  > "SCORES/NFIA_NFIA_Occupancy_down95bp_150bp_up95bp_150bp_read1_combined.cdt"
 
@@ -131,14 +128,12 @@ awk '
 '
 wc -l NFIA_Nuc_upstream_shuffled_temp.bed
 wc -l NFIA_Nuc_downstream_shuffled_temp.bed
-4029 NFIA_Nuc_upstream_shuffled_temp.bed
-3937 NFIA_Nuc_downstream_shuffled_temp.bed
 
 
 awk '{OFS="\t"} {print $1,$2,$3,$4,$5,$6,$7,$8,$9,"engageNuddown"}' NFIA_Nuc_downstream_shuffled_temp.bed  > NFIA_engagedNuc_shuffled_downstream.bed
 awk '{OFS="\t"} {print $1,$2,$3,$4,$5,$6,$7,$8,$9,"engageNudup"}' NFIA_Nuc_upstream_shuffled_temp.bed > NFIA_engagedNuc_shuffled_upstream.bed
 rm NFIA_Nuc_downstream_shuffled_temp.bed NFIA_Nuc_upstream_shuffled_temp.bed 
-## convert NFIA all to engage Nuc downstream
+## convert NFIA with randomized socre all to engage Nuc downstream
 
 awk '{ if ($6 == "+") print $0 >  "NFIA_Nuc_upstream_shuffled_+.bed"; else print $0 >  "NFIA_Nuc_upstream_shuffled_-.bed" }' NFIA_engagedNuc_shuffled_upstream.bed
 awk '{OFS="\t"} {print $1,$2,$3,$4,$5,"+",$7,$9,$8,$10}' NFIA_Nuc_upstream_shuffled_-.bed >  NFIA_Nuc_upstream_shuffled_-_convert.bed
@@ -154,7 +149,6 @@ rm NFIA_Nuc_upstream_shuffled_-_convert.bed NFIA_Nuc_upstream_shuffled_+_convert
 bedtools shift -i NFIA_shuffled_downNuc.bed -g $Genome -p 95 -m -95 > NFIA_shuffled_downNuc_down95.bed
 bedtools shift -i NFIA_shuffled_downNuc.bed -g $Genome -p -95 -m 95 > NFIA_shuffled_downNuc_up95.bed
 
-
 for file in NFIA_shuffled_downNuc_down95.bed NFIA_shuffled_downNuc_up95.bed ; do
   filename=$(basename "$file" ".bed")
   java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 150 $file -o ${filename}_150bp.bed
@@ -168,11 +162,11 @@ paste "SCORES/NFIA_NFIA_shuffled_downNuc_up95_150bp_read1_MIN100_combined_SCORES
 tail -n +2 | cut -f 2-3 | \
 paste "NFIA_shuffled_downNuc.bed" -  > NFIA_shuffled_downNuc_SCORES.bed
 rm NFIA_shuffled_downNuc_down95.bed NFIA_shuffled_downNuc_up95.bed
-
+## shift Orientated NFIA to downstream 125bp for better see 10bp pattern in heatmap
 bedtools shift -i NFIA_downNuc.bed -p 125 -m -125 -g $Genome > NFIA_downNuc_down125.bed
-
+## take Q4 of BX NFIA sites
 tail -n 1991 NFIA_downNuc.bed > NFIA_downNuc_4.bed
-
+## expand
 java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 NFIA_engageNucdown_NucSort.bed  -o $MOTIF/1000bp/NFIA_engageNucdown_NucSort_1000bp.bed
 java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 500  NFIA_downNuc.bed -o $MOTIF/1000bp/NFIA_downNuc_500bp.bed
 java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 250 NFIA_downNuc_down125.bed -o $MOTIF/1000bp/NFIA_downNuc_down125_250bp.bed
