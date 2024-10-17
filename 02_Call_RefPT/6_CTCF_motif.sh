5b706a3b92ddc8f98078ed30188108f772a9d4bd
HeninChen <46940074+HeninChen@users.noreply.github.com>
2024-10-17 02:25:51 -0400

    Update 6_CTCF_motif.sh
    makesure CTCF ($1 !~/alt/ && $1 !~/random/ && $1 !~/Un/ )

--- 02_Call_RefPT/6_CTCF_motif.sh
+++ 02_Call_RefPT/6_CTCF_motif.sh
@@ -4,7 +4,7 @@
 module load anaconda
 source activate bioinfo
 ### CHANGE ME
-WRK=/storage/group/bfp2/default/hxc585_HainingChen/Fox_NFIA_CTCF/
+WRK=/Path/to/Title
 MOTIF=$WRK/data/RefPT-Motif/
 FIMO=$WRK/02_Call_RefPT/FIMO
 SCRIPTMANAGER=$WRK/bin/ScriptManager-v0.15.jar
@@ -16,8 +16,14 @@ mkdir -p $WRK/Motif_analysis/CTCF
 
 cd $WRK/Motif_analysis/CTCF
 
-wc -l $FIMO/CTCF/CTCF_Occupancy_1bp.bed
+awk '{OFS="\t"} {print $1,$2,$3,$4,$7,$6,"K562_CTCF"}' $FIMO/CTCF/CTCF_Occupancy_1bp.bed  | awk '
+{
+    if ($1 !~/alt/ && $1 !~/random/ && $1 !~/Un/ ) {
+        print $0 > "CTCF_Occuoancy_1bp.bed";
+    } 
+}'
 
-awk '{OFS="\t"} {print $1,$2,$3,$4,$7,$6,"K562_CTCF"}' $FIMO/CTCF/CTCF_Occupancy_1bp.bed  > CTCF_Occuoancy.bed 
+wc -l CTCF_Occupancy_1bp.bed
+
 
 java -jar $SCRIPTMANAGER coordinate-manipulation expand-bed -c 1000 CTCF_Occuoancy.bed -o $MOTIF/1000bp/CTCF_Occuoancy_1000bp.bed
