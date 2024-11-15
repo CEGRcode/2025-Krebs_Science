d2e9d987ab2e7e40915aa4d124548c4cbabec4ef
Olivia Wen-Mei Lang <oliviawlang@gmail.com>
2024-11-15 12:46:38 -0500

    streamline 00_Download_and_Preprocessing for hg38
    streamline script headers and reformat for hg38 genome builds and by assay

--- 00_Download_and_Preprocessing/2_Merge_Replicates.sh
+++ 00_Download_and_Preprocessing/2_Merge_Replicates.sh
@@ -16,20 +16,26 @@ cd $WRK/../data/sample-BAM
 
 
 # Merged IgG (K562-BX) (HepG2-BX)
-java -jar $PICARD MergeSamFiles -I 33925_IgG_i5006_K562_-_IMDM_-_BX.bam \
-                                -I 34031_IgG_i5006_K562_-_IMDM_-_BX.bam \
-                                -I 34055_IgG_i5006_K562_-_IMDM_-_BX.bam \
-                                -I 37449_IgG_i5006_K562_-_IMDM_-_BX.bam \
-                                -I 34469_IgG_i5006_K562_-_IMDM_-_BX.bam \
-                                -I 36713_IgG_i5006_K562_-_IMDM_-_BX.bam  \
-                                -I 36900_IgG_i5006_K562_-_IMDM_-_BX.bam \
-                                -I 38288_IgG_i5006_K562_-_IMDM_-_BX.bam \
-                                -O K562_IgG_BX_merge_hg38.bam
+java -jar $PICARD MergeSamFiles -O K562_IgG_BX_merge_hg38.bam \
+        -I 33925_IgG_i5006_K562_-_IMDM_-_BX.bam \
+        -I 33963_IgG_i5006_K562_-_IMDM_-_BX.bam \
+        -I 34031_IgG_i5006_K562_-_IMDM_-_BX.bam \
+        -I 34055_IgG_i5006_K562_-_IMDM_-_BX.bam \
+        -I 34175_IgG_i5006_K562_-_IMDM_-_BX.bam \
+        -I 34469_IgG_i5006_K562_-_IMDM_-_BX.bam \
+        -I 36713_IgG_i5006_K562_-_IMDM_-_BX.bam \
+        -I 36900_IgG_i5006_K562_-_IMDM_-_BX.bam \
+        -I 37449_IgG_i5006_K562_-_IMDM_-_BX.bam \
 
-java -jar $PICARD MergeSamFiles -I 38481_IgG_i5006_HepG2_-_-_-_BX.bam \
-                                -I 38482_IgG_i5006_HepG2_-_-_-_BX.bam \
-                                -I 38483_IgG_i5006_HepG2_-_-_-_BX.bam \
-                                -O HepG2_IgG_BX_merge_hg38.bam
+        # -I 34668_IgG_i5006_K562_-_IMDM_-_BX.bam \
+        # -I 38288_IgG_i5006_K562_-_IMDM_-_BX.bam
+# HAINING QUESTION: What are these samples doing?
+
+
+java -jar $PICARD MergeSamFiles -O HepG2_IgG_BX_merge_hg38.bam \
+        -I 38481_IgG_i5006_HepG2_-_-_-_BX.bam \
+        -I 38482_IgG_i5006_HepG2_-_-_-_BX.bam \
+        -I 38483_IgG_i5006_HepG2_-_-_-_BX.bam
 
 # Use Picard to merge resequenced technical replicates, otherwise rename BAM using cp
 
@@ -39,21 +45,24 @@ cp 28326_Input_-_K562_-_-_50Unuclease10min-0cycSonic-newQuenchingbuffer_BI.bam K
 # CTCF
 cp 33924_CTCF_07-729_K562_-_IMDM_-_BX.bam K562_CTCF_BX_rep1_hg38.bam
 cp 34174_CTCF_07-729_K562_-_IMDM_-_BX.bam K562_CTCF_BX_rep2_hg38.bam
+
 # RAD21
-java -jar $PICARD MergeSamFiles -I 32070_RAD21_HPA020044_K562_-_-_-_BX.bam \
-                                -I 32119_RAD21_HPA020044_K562_-_-_-_BX.bam \
-                                -O K562_RAD21_BX_rep1_hg38.bam
-cp 34621_RAD21_HPA020044_K562_-_-_-_BX.bam K562_RAD21_BX_rep2_hg19.bam
+java -jar $PICARD MergeSamFiles -O K562_RAD21_BX_rep1_hg38.bam \
+        -I 32070_RAD21_HPA020044_K562_-_-_-_BX.bam \
+        -I 32119_RAD21_HPA020044_K562_-_-_-_BX.bam
+
+cp 34621_RAD21_HPA020044_K562_-_-_-_BX.bam K562_RAD21_BX_rep2_hg38.bam
 # SMC3
 cp 34622_Smc3_ab9263_K562_-_-_-_BX.bam K562_SMC3_BX_rep1_hg38.bam
 cp 38476_Smc3_ab9263_K562_-_IMDM_-_BX.bam K562_SMC3_BX_rep2_hg38.bam
 
 
 # NFIA
-java -jar $PICARD MergeSamFiles -I 32067_NFIA_HPA008884_K562_-_-_-_BX_FilteredBAM.bed \
-                                -I 32116_NFIA_HPA008884_K562_-_-_-_BX_FilteredBAM.bed \
-                                -O K562_NFIA_BX_rep1_hg38.bam
-cp 34598_NFIA_HPA008884_K562_-_-_-BX_FilteredBAM.bed K562_NFIA_BX_rep2_hg38.bam
+java -jar $PICARD MergeSamFiles -O K562_NFIA_BX_rep1_hg38.bam \
+        -I 32067_NFIA_HPA008884_K562_-_-_-_BX.bed \
+        -I 32116_NFIA_HPA008884_K562_-_-_-_BX.bed
+
+cp 34598_NFIA_HPA008884_K562_-_-_-BX.bed K562_NFIA_BX_rep2_hg38.bam
 
 # FoxA1 HepG2, K562
 cp 32354_FoxA1_ab23738_HepG2_-_-_-_BX.bam HepG2_FOXA1_BX_rep1_hg38.bam
