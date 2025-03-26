18202716aba0d34db255d087a794be8662c236c2
HeninChen <46940074+HeninChen@users.noreply.github.com>
2025-03-26 05:19:35 -0400

    move it to bulk processing

--- X_Bulk_Processing/11_cut-site_nucleotide-content.sh
+++ X_Bulk_Processing/11_cut-site_nucleotide-content.sh
@@ -3,8 +3,8 @@
 # Perform sequence content analysis around cut sites
 
 ### CHANGE ME
-WRK=/path/to/2024-Krebs_Science/Z_Figures
-WRK=/storage/group/bfp2/default/owl5022-OliviaLang/2024-Krebs_Science/Z_Figures
+WRK=/storage/group/bfp2/default/hxc585_HainingChen/2025_Chen_TF-Nuc/X_Bulk_Processing
+#WRK=/storage/group/bfp2/default/owl5022-OliviaLang/2024-Krebs_Science/0X_Bulk_Processing
 ###
 
 # Dependencies
@@ -19,7 +19,8 @@ source activate /storage/group/bfp2/default/owl5022-OliviaLang/conda/bx
 
 UPDOWN_KMER=../bin/updownstream_di-nt_tally.py
 KMER2NT=../bin/dint_to_nt_positional_count_matrix.py
-STACKNT=../bin/make_NT_stack_barchart_TSV.py
+STACKNT=../bin/make_stack_barchart_TSV.py
+SPLIT_KMER=../bin/split_kmer_to_composite.py
 
 BAMDIR=../data/BAM
 
@@ -42,17 +43,20 @@ TEMP=UpstreamKmerAnalysis
 # samtools flagstat $BAMDIR/MNase-seq_21U_rep1_hg38.bam         > MNase-seq_21U_rep1_hg38.bam.flagstat
 # samtools flagstat $BAMDIR/MNase-seq_79U_rep1_hg38.bam         > MNase-seq_79U_rep1_hg38.bam.flagstat
 # samtools flagstat $BAMDIR/MNase-seq_304U_rep1_hg38.bam        > $TEMP/BAM/MNase-seq_304U_rep1_hg38.bam.flagstat
+#samtools flagstat $BAMDIR/MPE-seq_20min_rep2_mm10.bam > $TEMP/BAM/MPE-seq_20min_rep2_mm10.bam.flagstat
 
 # Subsample BAM - hardcoded this for different BAM files (Seed=2)
-# samtools view -b -s 2.0001 $BAMDIR/MNase-seq_21U_rep1_hg38.bam        > $TEMP/BAM/MNase-seq_21U_rep1_hg38_SUBSAMPLE.bam # 170M --> 17K reads reads
-# samtools view -b -s 2.002 $BAMDIR/DNase-seq_ENCFF425WDA_rep1_hg38.bam    > $TEMP/BAM/DNase-seq_ENCFF425WDA_rep1_hg38_SUBSAMPLE.bam  # 18M --> 35K reads
-# samtools view -b -s 2.0002 $BAMDIR/DNase-seq_ENCFF518XTC_rep1_hg38.bam    > $TEMP/BAM/DNase-seq_ENCFF518XTC_rep1_hg38_SUBSAMPLE.bam  # 98M --> 19K reads
-# samtools view -b --threaded 4 -s 2.00001 $BAMDIR/BNase-seq_50U-10min_merge_hg38.bam > $TEMP/BAM/BNase-seq_50U-10min_merge_hg38_SUBSAMPLE.bam # 1.3B --> 12K reads
-# samtools view -b -s 2.001 $BAMDIR/BNase-seq_50U-10min_1_hg38.bam > $TEMP/BAM/BNase-seq_50U-10min_1_hg38_SUBSAMPLE.bam # 22M --> 22K reads
+# samtools view -b -s 2.0001 $BAMDIR/MNase-seq_21U_rep1_hg38.bam        > $TEMP/BAM/MNase-seq_21U_rep1_hg38_SUBSAMPLE.bam # 170M --> 17K reads
+# samtools view -b -s 2.002 $BAMDIR/DNase-seq_ENCFF425WDA_rep1_hg38.bam    > $TEMP/BAM/DNase-seq_ENCFF425WDA_rep1_hg38_SUBSAMPLE.bam  # 18M --> 
+samtools view -b -s 2.0002 $BAMDIR/DNase-seq_ENCFF518XTC_rep1_hg38.bam    > $TEMP/BAM/DNase-seq_ENCFF518XTC_rep1_hg38_SUBSAMPLE.bam  # 98M -->  
+samtools view -b -s 2.0001 $BAMDIR/BNase-seq_50U-10min_merge_hg38.bam > $TEMP/BAM/BNase-seq_50U-10min_merge_hg38_SUBSAMPLE.bam # 1.3B --> 10K reads
+#samtools view -b -s 2.0001 $TEMP/BAM/MPE-seq_20min_rep2_mm10.bam > $TEMP/BAM/MPE-seq_20min_rep2_mm10_SUBSAMPLE.bam
+
+
 # samtools view -b -s 2.002 $BAMDIR/BNase-seq_50U-30min_merge_hg38.bam > $TEMP/BAM/BNase-seq_50U-30min_merge_hg38_SUBSAMPLE.bam # 13M --> 26K reads
 
 # Add your BAM files here
-for BAMFILE in "${BAMDIR}/BNase-seq_50U-10min_1_hg38.bam" "${BAMDIR}/MNase-seq_21U_rep1_hg38.bam" "${BAMDIR}/BNase-seq_50U-30min_merge_hg38.bam" "${BAMDIR}/DNase-seq_ENCFF425WDA_rep1_hg38.bam" "${BAMDIR}/DNase-seq_ENCFF518XTC_rep1_hg38.bam";
+for BAMFILE in "${BAMDIR}/BNase-seq_50U-10min_1_hg38.bam" "${BAMDIR}/MNase-seq_21U_rep1_hg38.bam" "${BAMDIR}/BNase-seq_50U-30min_merge_hg38.bam" "${BAMDIR}/MPE-seq_20min_rep2_mm10.bamm" ;
 do
     BAM=`basename $BAMFILE ".bam"`
 
