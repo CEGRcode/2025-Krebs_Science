011df5a9bcb66e8de6fec3128547113a7236f9a5
HeninChen <46940074+HeninChen@users.noreply.github.com>
2026-04-22 13:16:28 -0400

    Update make_violin_plot.py
    update violin

--- bin/make_violin_plot.py
+++ bin/make_violin_plot.py
@@ -1,112 +1,74 @@
-#!/usr/bin/env python
-from os.path import splitext
-import sys, re, argparse, random
-import matplotlib.pyplot as plt
+#!/usr/bin/env python3
+
+import sys
+import numpy as np
 import pandas as pd
 import seaborn as sns
+import matplotlib.pyplot as plt
 
-# Use text, not shapes of text in SVG
-plt.rcParams['svg.fonttype'] = 'none'
+# -----------------------
+# INPUT
+# -----------------------
+infile = sys.argv[1]
+outfile = sys.argv[2] if len(sys.argv) > 2 else "violin.svg"
 
-# Python 3.6+
-# relies on dict insertion order
+# -----------------------
+# LOAD DATA
+# -----------------------
+data = pd.read_csv(infile, sep="\t", header=None, names=["value", "group"])
 
-# Check Matplotlib colors when building your config files: https://matplotlib.org/stable/gallery/color/named_colors.html
+# remove zeros (log safety)
+data = data[data["value"] > 0]
 
-def getParams():
-	'''Parse parameters from the command line'''
-	parser = argparse.ArgumentParser(description='')
+# log10 transform
+data["value"] = np.log10(data["value"])
 
-	parser.add_argument('-i','--input', metavar='two_col_file', dest='data_file', required=False, default=None, help='tab-delimited file made of two columns: first column y values to plot (must all be numeric values), second column is the grouping (which violin group along x-axis to contribute to)')
-	parser.add_argument('--width', metavar='width', required=False, type=int, default=8, help='width of figure')
-	parser.add_argument('--height', metavar='height', required=False, type=int, default=4, help='height of figure')
-	parser.add_argument('--title', metavar='title', required=False, default=None, help='title of figure')
-	parser.add_argument('--xlabel', metavar='xlabel', required=False, default=None, help='x-axis label')
-	parser.add_argument('--ylabel', metavar='ylabel', required=False, default=None, help='y-axis label')
-	parser.add_argument('--preset1', action='store_true', help='use proximal/distal presets for nucleosome intervals (Fig 4d)')
-	parser.add_argument('--preset2', action='store_true', help='use proximal/distal presets for half-nucleosome intervals (Fig 4d)')
-	parser.add_argument('-o','--output', metavar='output_svg', dest='output_svg', required=False, default=None, help='name of SVG filepath to save figure to (if none provided, figure pops up in new window)')
-
-	args = parser.parse_args()
-	return(args)
-
-
-preset1_order = [
-	"H2AZ-H2B_Proximal", "H2AZ-H2B_Distal",
-	"H3K4me3-H3_Proximal", "H3K4me3-H3_Distal",
-	"H3K9ac-H3_Proximal", "H3K9ac-H3_Distal",
-	"H3K27ac-H3_Proximal", "H3K27ac-H3_Distal"
-]
-preset2_order = [
-	"H3K4me3-H3_Proximal", "H3K4me3-H3_Distal",
-	"H3K9ac-H3_Proximal", "H3K9ac-H3_Distal",
-	"H3K27ac-H3_Proximal", "H3K27ac-H3_Distal"
+# -----------------------
+# ORDER + COLORS
+# -----------------------
+order = [
+    "MNase-Overlap",
+    "MNase-NoOverlap",
+    "BNase-Overlap",
+    "BNase-NoOverlap"
 ]
 
-# Example Data:
-# 0.550716	category1
-# 0.493109	category3
-# 0.401034	category2
-# 0.498233	category1
-# 0.579172	category3
-# 0.658480	category1
-# 0.386018	category3
-# 0.464670	category1
-# 0.481569	category2
+palette = {
+    "MNase-Overlap": "#0000FF",
+    "MNase-NoOverlap": "#FF0000",
+    "BNase-Overlap": "#0000FF",
+    "BNase-NoOverlap": "#FF0000"
+}
 
-if __name__ == "__main__":
-	'''Plot violin plot'''
-	args = getParams()
+# -----------------------
+# PLOT
+# -----------------------
+plt.figure(figsize=(5, 4))
 
-	# Load data
-	data = pd.read_csv(args.data_file, sep="\t", header=None, names=['y','category'])
-	print(data)
+ax = sns.violinplot(
+    data=data,
+    x="group",
+    y="value",
+    order=order,
+    palette=palette,
+    inner="quartile",
+    cut=0,
+    linewidth=0.8
+)
 
-	# Initialize plot base with user-specified size
-	fig, ax= plt.subplots()
-	plt.figure(figsize=(args.width, args.height))
+# -----------------------
+# FORMATTING
+# -----------------------
+ax.set_xlabel("")
+ax.set_ylabel("Tag Occupancy (log10)")
+ax.set_ylim(0, 4)
 
-	# Create red/blue pallette for proximal/distal
-	hardcoded_pal = {
-		"H2AZ-H2B_Proximal": "blue", "H2AZ-H2B_Distal": "red",
-		"H3K4me3-H3_Proximal": "blue", "H3K4me3-H3_Distal": "red",
-		"H3K9ac-H3_Proximal": "blue", "H3K9ac-H3_Distal": "red",
-		"H3K27ac-H3_Proximal": "blue", "H3K27ac-H3_Distal": "red"
-	}
+ax.tick_params(axis='x', rotation=45)
 
-	custom_order = sorted(list(data['category'].unique()))
-	custom_pal = None
-	if (args.preset1):
-		custom_order = preset1_order
-		custom_pal = hardcoded_pal
-	elif (args.preset2):
-		custom_order = preset2_order
-		custom_pal = hardcoded_pal
+plt.tight_layout()
 
-	# Scatter Plot or other type of plot here!!!!!
-	# could swap out for 'swarmplot' and google seaborn library for others
-	ax = sns.violinplot(x="category", y="y", data=data, cut=0, linewidth=0.5, palette=custom_pal, order=custom_order)
-
-	# Format x-axis tickmarks
-	ax.tick_params(axis='x', rotation=90)
-
-	# Add title
-	if (args.title!=None):
-		ax.set_title(args.title)
-	# Label x-axis
-	if (args.xlabel!=None):
-		plt.xlabel(args.xlabel)
-	# Label y-axis
-	if (args.ylabel!=None):
-		plt.ylabel(args.ylabel)
-
-	# Output...
-	if(args.output_svg==None):
-		plt.show()
-	elif(splitext(args.output_svg)[-1]!=".svg"):
-		sys.stderr.write("Please use SVG file extension to save output!\n")
-		plt.savefig(args.output_svg, transparent=True)
-		plt.show()
-	else:
-		plt.savefig(args.output_svg, transparent=True)
-		sys.stderr.write("SVG written to %s\n" % args.output_svg)
+# -----------------------
+# OUTPUT
+# -----------------------
+plt.savefig(outfile, dpi=300, transparent=True)
+print(f"Saved: {outfile}")
